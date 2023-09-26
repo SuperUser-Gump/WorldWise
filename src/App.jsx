ac3638fc9f6ef4ef6587cdc1ad8e06e4cafbee4b
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-26 21:27:00 +0700

    feat: Add '/login' route
    - Set the path '/login' to display the Login component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
