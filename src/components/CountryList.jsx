477f73116952be2c5e5758d9977d21903c00edb1
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-28 03:08:06 +0700

    feat: Create CountryList component
    - Created the CountryList component to display a list of countries.
    - Replace the route path 'countries' of the 'app' route placeholder component with the CountryList component.

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner.jsx";
+import Message from "./Message.jsx";
+import CountryItem from "./CountryItem.jsx";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = [];
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
