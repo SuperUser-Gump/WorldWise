fcebe5b5659b37ee66dba41fe6e577842213c74c
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-28 01:28:51 +0700

    feat: Add nested routes under 'app' route
    - Added nested routes 'cities', 'countries', and 'form' under the 'app' route, each displaying placeholders.
    - Rendered components for 'cities', 'countries', and 'form' routes within the Outlet component within the Sidebar.
    - Set the list of cities as the index route for 'app'.
    - Set HomePage as the index route.

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/pricing" element={<Pricing />} />
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo.jsx";
 import AppNav from "./AppNav.jsx";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer.jsx";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
+      <Outlet />
 
-      <p>List of cities</p>
+      <Footer />
     </div>
   );
 }
