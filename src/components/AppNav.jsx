e89fb644685619428d2a747896e28a494cb77b33
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-28 01:37:18 +0700

    feat: Replace AppNav placeholder with NavLink components for 'cities' and 'countries' routes
    - Replaced the placeholder in AppNav with NavLink components.

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 
 function AppNav() {
-  return <div>AppNav</div>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
