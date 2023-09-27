3ab3bcbb098386d50a4139b2f78684685c428705
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-28 02:39:24 +0700

    feat: Create CityItem component to display city details
    - Created the CityItem component to receive a city prop and display city details, including the country flag, city name, date, and a delete button.

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+function CityItem({ city }) {
+  const { cityName, emoji, date } = city;
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
+}
+
+export default CityItem;
