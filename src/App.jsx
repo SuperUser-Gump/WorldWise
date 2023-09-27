f346a28c26ccfcb23732e3cd8aff7a832818f570
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-28 01:57:49 +0700

    feat: Create CityList component
    - Created the CityList component to display a list of cities.
    - Replaced the index route of the 'app' route placeholder component with the CityList component.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Pricing from "./pages/Pricing.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import Login from "./pages/Login.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
+import CityList from "./components/CityList.jsx";
 
 function App() {
   return (
@@ -16,8 +17,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CityList.module.css";
+
+function CityList() {
+  return <ul className={styles.cityList}>LIST</ul>;
+}
+
+export default CityList;
