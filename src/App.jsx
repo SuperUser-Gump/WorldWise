36ced3b10e84dd2da8657a5d485b8350ddcf1358
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-29 02:50:29 +0700

    feat: Complete fetching cities data from API feature
    - Successfully fetched cities data from an external API source.
    - Implemented the rendering of cities data in relevant components.
    - Squash merged the 'feature/fetch-cities-data' branch into the 'master' branch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/Product.jsx";
@@ -8,7 +8,28 @@ import Login from "./pages/Login.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import CityList from "./components/CityList.jsx";
 
+const BASE_URL = "http://localhost:8000";
+
 function App() {
+  const [cities, setCities] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -17,8 +38,14 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner.jsx";
+import CityItem from "./CityItem.jsx";
+import Message from "./Message.jsx";
 
-function CityList() {
-  return <ul className={styles.cityList}>LIST</ul>;
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
