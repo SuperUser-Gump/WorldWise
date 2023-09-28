76549e347a44f4ec4ebe34494078873efd2d74e6
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-28 12:12:17 +0700

    feat: Add query strings 'lat' and 'lng' to Link in CityItem component
    - Enhanced the Link component within the CityItem component to include query strings 'lat' and 'lng', allowing users to set latitude and longitude when clicking on the item.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -9,11 +9,14 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 function CityItem({ city }) {
-  const { cityName, emoji, date, id } = city;
+  const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
