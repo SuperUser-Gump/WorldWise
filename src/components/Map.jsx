182d2ca3ac87ae3fe662f2ddccb2c2bcbdeee3b7
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-28 12:14:18 +0700

    feat: Read query segments 'lat' and 'lng', add button to update values
    - Read the query segments 'lat' and 'lng' from the URL and store them in 'lat' and 'lng' variables.
    - Rendered the 'lat', and 'lng' data to display the retrieved information.
    - Added a button to update the values of 'lat' and 'lng' query parameters.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
