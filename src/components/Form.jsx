1a0ed9411a1db8fe35a4edf340eff56eea8de64e
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-29 02:10:56 +0700

    feat: Replace add and back buttons in Form with Button component
    - Replaced the add button in the Form component with a Button component of type primary.
    - Replaced the back button in the Form component with a Button component of type back.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import styles from "./Form.module.css";
+import Button from "./Button.jsx";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -49,8 +50,8 @@ function Form() {
       </div>
 
       <div className={styles.buttons}>
-        <button>Add</button>
-        <button>&larr; Back</button>
+        <Button type="primary">Add</Button>
+        <Button type="back">&larr; Back</Button>
       </div>
     </form>
   );
