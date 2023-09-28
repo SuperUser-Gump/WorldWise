b8f953e302e3a6d8d781d2279a44f8b3153675ab
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-28 11:45:20 +0700

    feat: Add dynamic path "id" to "cities" route and Link in CityItem component
    - Updated the "cities" route to include a dynamic path segment ":id" for displaying the City component with a corresponding ID.
    - Added a Link component within the CityItem component to link to the dynamic path ":id" when an item is clicked.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -8,14 +9,16 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link className={styles.cityItem} to={`${id}`}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
