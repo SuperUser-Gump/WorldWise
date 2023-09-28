9f8b717a64f5915ce8453036fff698bef73e741e
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-29 02:34:46 +0700

    feat: Update 'app' route to use Navigate
    - Updated the 'cities' route path of the 'app' route to use the Navigate component, ensuring immediate redirection to 'app/cities' when hitting the 'app' route.
    - Added the 'replace' prop to the Navigate component to fix navigation issues, allowing users to navigate back to the previous history stack correctly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
@@ -41,10 +41,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
