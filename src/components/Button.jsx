7f8c6af8b636c002dfc7c7d4e1b9495b67bb3e4a
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-29 02:10:01 +0700

    feat: Create Button component with children, onClick, and type props
    - Created the Button component with support for children, onClick, and type props.

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
