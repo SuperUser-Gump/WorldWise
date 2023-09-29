bc415e614bcda965972607a63075332babda8eee
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-29 11:28:05 +0700

    feat: Consume CitiesContext in CityList and CountryList components
    - Updated the CityList and CountryList components to consume the CitiesContext, enabling them to receive and utilize the cities and isLoading states.

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,8 +3,11 @@ import styles from "./CityList.module.css";
 import Spinner from "./Spinner.jsx";
 import CityItem from "./CityItem.jsx";
 import Message from "./Message.jsx";
+import { useCities } from "../../contexts/CitiesContext.jsx";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
