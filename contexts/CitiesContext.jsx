d69d0ba1bfa6b6553d39f32c8f9c9008fa01b24e
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-29 12:03:15 +0700

    feat: Add currentCity and getCity to CitiesContext
    - Added currentCity state to store the currently selected city.
    - Added getCity function within the CitiesContext to fetch city data by ID.

--- contexts/CitiesContext.jsx
+++ contexts/CitiesContext.jsx
@@ -7,6 +7,7 @@ const BASE_URL = "http://localhost:8000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -25,8 +26,21 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
