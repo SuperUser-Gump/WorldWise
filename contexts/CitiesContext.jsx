673c8cfad29cd6ebaf0909a33305599856b93c99
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-29 11:12:46 +0700

    feat: Create CitiesProvider with isLoading and cities props
    - Created the CitiesProvider component to provide 'isLoading' and 'cities' props to its children.

---
+++ contexts/CitiesContext.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+
+const BASE_URL = "http://localhost:8000";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
