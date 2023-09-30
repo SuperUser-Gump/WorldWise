6eb3665ac698968da56982367bde1ba1ab17e134
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-30 18:33:27 +0700

    refactor: Add useReducer for state management and refactor actions
    - Set up the useReducer hook with an initialState and a reducer function for improved state management.
    - Added an error state with a rejected action type to handle error scenarios.
    - Refactored setIsLoading to loading.
    - Refactored setCities to cities/loaded.
    - Refactored setCurrentCity to city/loaded.
    - Refactored the functionality for adding a city to a list to city/created.
    - Refactored the functionality for removing a city from a list to city/deleted.

--- contexts/CitiesContext.jsx
+++ contexts/CitiesContext.jsx
@@ -1,13 +1,63 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
 
 const CitiesContext = createContext();
-
 const BASE_URL = "http://localhost:8000";
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+
+    case "cities/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+
+    case "city/loaded":
+      return { ...state, isLoading: false, currentCity: action.payload };
+
+    case "city/created":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+
+    case "city/deleted":
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+      };
+
+    case "rejected":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(function () {
     async function fetchCities() {
