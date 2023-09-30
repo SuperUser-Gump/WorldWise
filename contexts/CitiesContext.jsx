5d77f2cdd4a0ee4b8e0eeb76c138ea8fc24a142a
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-30 16:28:24 +0700

    feat: Add sending POST request with selected city
    - Added functionality to send a POST request with the selected city information.
    - Updated the cities state with the result obtained from the POST request.

--- contexts/CitiesContext.jsx
+++ contexts/CitiesContext.jsx
@@ -39,8 +39,28 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+
+      setCities((cities) => [...cities, data]);
+    } catch (err) {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+    <CitiesContext.Provider
+      value={{ cities, isLoading, currentCity, getCity, createCity }}
+    >
       {children}
     </CitiesContext.Provider>
   );
