4140c6b4ec243150a0e4a347a16c76b1d8545472
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-30 16:07:17 +0700

    feat: Add React Date Picker component
    - Installed the React Date Picker library.
    - Added the DatePicker component within the Form to allow users to select dates.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,6 +7,8 @@ import { useUrlPosition } from "../../hooks/useUrlPosition.js";
 import Message from "./Message.jsx";
 import Spinner from "./Spinner.jsx";
 import BackButton from "./BackButton.jsx";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -81,10 +83,11 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        <DatePicker
           id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat="dd/MM/yyyy"
         />
       </div>
 
