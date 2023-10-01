9dab3da491a78133aea60541959a8b0fa3ed9900
Krittin Saenpakdi <gump29062@gmail.com>
2023-10-01 16:22:26 +0700

    feat: Complete adding route protection to guard against unauthorized access
    - Successfully adding route protection to guard against unauthorized access functionality.
    - Squash merged the 'feature/route-protection' branch.

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
 import { CitiesProvider } from "../contexts/CitiesContext.jsx";
 import { AuthProvider } from "../contexts/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 
 function App() {
   return (
@@ -23,7 +24,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
