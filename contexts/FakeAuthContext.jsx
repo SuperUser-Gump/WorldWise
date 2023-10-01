64d09cb3359f1330c0108b3a8b14b7a2144d27a9
Krittin Saenpakdi <gump29062@gmail.com>
2023-10-01 14:48:32 +0700

    feat: Add useReducer for authentication management
    - Set up the useReducer hook for handling authentication state.
    - Added user and isAuthenticated as initial state values.
    - Added login and logout actions for managing user authentication.
    - Added the login and logout functionality using dispatch actions.

--- contexts/FakeAuthContext.jsx
+++ contexts/FakeAuthContext.jsx
@@ -1,13 +1,49 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticating: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticating: false };
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 function AuthProvider({ children }) {
-  function login(email, password) {}
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
 
-  function logout() {}
+  function logout() {
+    dispatch({ type: "logout" });
+  }
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuht() {
@@ -16,3 +52,5 @@ function useAuht() {
     throw new Error("AuthContext was used outside AuthProvider");
   }
 }
+
+export { AuthProvider, useAuht };
