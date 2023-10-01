0348666b08491a1725c3876cc8a96f985de823d6
Krittin Saenpakdi <gump29062@gmail.com>
2023-10-01 15:37:12 +0700

    feat: Add Login component
    - Replaced the FAKE_USER object with the user data retrieved from useAuth.
    - Added the Login component within the AppLayout component.

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Sidebar from "../components/Sidebar.jsx";
 import Map from "../components/Map.jsx";
 import styles from "./AppLayout.module.css";
+import User from "../components/User.jsx";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
