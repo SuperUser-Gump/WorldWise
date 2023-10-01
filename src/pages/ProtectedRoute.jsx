5ea1056eac95f9f62a9d1922074223b246794722
Krittin Saenpakdi <gump29062@gmail.com>
2023-10-01 16:19:19 +0700

    feat: Added ProtectedRoute for authorization control
    - Created the ProtectedRoute component to redirect unauthorized users to the home page ("/").
    - Wrapped the AppLayout component with the ProtectedRoute component within the App component.

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { useAuth } from "../../contexts/FakeAuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated) {
+        navigate("/");
+      }
+    },
+    [isAuthenticated, navigate]
+  );
+
+  return isAuthenticated ? children : null;
+}
+
+export default ProtectedRoute;
