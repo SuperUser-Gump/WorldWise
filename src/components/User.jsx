56d0a8f43c7d09e06ca72404d792d83a7e6743b7
Krittin Saenpakdi <gump29062@gmail.com>
2023-10-01 16:01:44 +0700

    feat: Complete implementing logout
    - Successfully implementing logout functionality.
    - Squash merged the 'feature/logout' branch.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,15 @@
 import styles from "./User.module.css";
 import { useAuth } from "../../contexts/FakeAuthContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 function User() {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
-  function handleClick() {}
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
