5fee09a352f8840d7a63db954698e44de4a021e8
Krittin Saenpakdi <gump29062@gmail.com>
2023-10-02 15:50:47 +0700

    perf: Add lazy loading for Homepage, Product, Pricing, PageNotFound, and Login components
    - Added lazy loading for the Homepage, Product, Pricing, PageNotFound, and Login components to improve initial page load performance by loading these components only when they are needed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { lazy } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage.jsx";
-import Product from "./pages/Product.jsx";
-import Pricing from "./pages/Pricing.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import Login from "./pages/Login.jsx";
+
+import { CitiesProvider } from "../contexts/CitiesContext.jsx";
+import { AuthProvider } from "../contexts/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import CityList from "./components/CityList.jsx";
 import CountryList from "./components/CountryList.jsx";
 import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
-import { CitiesProvider } from "../contexts/CitiesContext.jsx";
-import { AuthProvider } from "../contexts/FakeAuthContext.jsx";
-import ProtectedRoute from "./pages/ProtectedRoute.jsx";
+
+const Homepage = lazy(() => import("./pages/Homepage.jsx"));
+const Product = lazy(() => import("./pages/Product.jsx"));
+const Pricing = lazy(() => import("./pages/Pricing.jsx"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
 
 function App() {
   return (
