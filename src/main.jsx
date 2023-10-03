01e6746a763b6f7d98c710720e319522b7fddc94
Krittin Saenpakdi <gump29062@gmail.com>
2023-10-03 15:58:21 +0700

    chore: Add error boundary with react-error-boundary
    - Installed the react-error-boundary library to improve error handling and debugging.
    - Utilized the ErrorBoundary component to wrap App component and gracefully display the ErrorFallback component as a fallback when errors occur.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./components/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,7 @@
 export default function PageNotFound() {
   return (
     <div>
-      <h1>Page not found 😢</h1>
+      <h1 style={{ color: "black" }}>Page not found 😢</h1>
     </div>
   );
 }
